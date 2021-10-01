d53e1e322f927a92d1447475fbb3ccd09deec065
VProfirov <Vasil.Profirov@gmail.com>
2021-10-01 11:29:55 +0300

    01.HW-Ubuntu&openSUSE - working on

---
+++ 01.02.HW.Vagrant-and-VirtualBox_Ubuntu/vagrant/READ.md
@@ -0,0 +1,5 @@
+### New directory & file tree for the Vagrant spin-up
+
+idea: 
+- .vagrant -> uses the virtualbox file-system of the machines
+- vagrant -> uses the data needed from Vagrantfile to spin-up the machines
---
+++ 01.02.HW.Vagrant-and-VirtualBox_Ubuntu/vagrant/machines/db/provision/Ubuntu/db.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "* Add hosts ..."
+echo "192.168.99.100 db.dob.lab db" | tee -a /etc/hosts
+echo "192.168.99.101 web.dob.lab web" | tee -a /etc/hosts
+
+echo "* Install Software ..."
+apt-get update -y && apt-get upgrade -y
+apt-get install -y mariadb-server
+
+echo "* Adjust MariaDB connectivity ..."
+sudo sed -i.bak s/127.0.0.1/0.0.0.0/g /etc/mysql/mariadb.conf.d/50-server.cnf
+systemctl restart mariadb
+
+echo "* Create and load the database ..."
+mysql -u root < /vagrant/db/db_setup.sql
