cff6e64ae37d2cc520734dc9f68066cc2f41b418
VProfirov <Vasil.Profirov@gmail.com>
2021-10-14 17:03:07 +0300

    docker basics prov-script fix

--- 02.00.study-Containerization-with-Docker/vagrant/provision/docker-provision.sh
+++ 02.00.study-Containerization-with-Docker/vagrant/provision/docker-provision.sh
@@ -1,40 +1,50 @@
 #!/usr/bin/env bash
+# SYSTEM PROVISION PHASE
+echo "\\r\\n ** Addhosts..."
+echo "192.168.99.100 docker.dob.lab docker" | tee -a /etc/hosts
 
-echo '** Clean pre-existing docker packages'
-        echo '--> check: pre-existance'
+# APPLICATION PROVISION PHASE
+echo "\\r\\n ** Clean pre-existing docker packages"
+        echo "\\r\\n --> check: pre-existance"
         rpm -qa | grep -iE "podman|buildah|runc"
+echo "\\r\\n --> action: clean-up"
+sudo dnf remove -y podman buildah runc
+# sudo dnf remove podman runc
 
-sudo dnf remove podman buildah runc
+echo "y"
 
-echo '** Enable docker repo'
+echo "\r\n ** Enable docker repo"
 sudo dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
-echo '** Trying to remove pre-existing docker packages'
-sudo dnf remove docker docker-client docker-client-latest \
-                docker-common docker-latest docker-latest-logrotate \
-		remove docker docker-client docker-client-latest \
-                docker-logrotate docker-selinux docker-engine-selinux docker-engine
+# echo "** Trying to remove pre-existing docker packages"
+# sudo dnf remove docker docker-client docker-client-latest \
+#                 docker-common docker-latest docker-latest-logrotate \
+# 		remove docker docker-client docker-client-latest \
+#                 docker-logrotate docker-selinux docker-engine-selinux docker-engine
 
-echo '** Installing docker packages'
-sudo dnf install docker-ce docker-cli-ce
+echo "\r\n ** Installing docker packages"
+sudo dnf install -y docker-ce docker-ce-cli
 
 
-echo '--> check: containerd.io is installed'
+echo "\r\n --> check: containerd.io is installed"
 if [ -z "$(rpm -qa | grep -iE "^containerd.io")" ]; then
-        echo '*** containerd.io install'
+        echo "\r\n --> action: containerd.io install"
         sudo dnf install https://download.docker.com/linux/centos/7/x86_64/stable/Packages\
                         /containerd.io-1.4.9-3.1.el7.x86_64.rpm
 
                         #TODO: Do a webaddress check for the latest rpm (wget -> grep -iE "containerd.io-*" -> sort -r -> sed/awk {take the first of reversed-list})
 fi
 
-echo '** Enable + Start Docer'
+echo "\r\n ** Enable + Start Docer"
 for action in {status,enable,start,status}; do
+        echo "\r\n --> action: ${action}"
         sudo systemctl $action docker;
         # TODO: maybe more adequate checks
+        # NOTE: vagrant can blow an error because of the "systemctl status ..." needing a "quit" action
 done;
 
-echo '** Adding the $USER (vagrant) to the docker Grouop'
-sudo getent group docker;
-sudo usermod -aG docker $USER;
-sudo getent group docker;
+echo "\r\n ** Adding the $USER (vagrant) to the docker Grouop"
+getent group docker;
+# sudo usermod -aG docker $USER;
+sudo usermod -aG docker vagrant;
+getent group docker;
