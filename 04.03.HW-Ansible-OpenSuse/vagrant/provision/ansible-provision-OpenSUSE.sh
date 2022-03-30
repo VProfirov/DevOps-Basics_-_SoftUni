7eba9ab593eb5e0c0091e33c7c0f6172c42c0bec
VProfirov <Vasil.Profirov@gmail.com>
2022-03-30 21:08:22 +0300

    ansible ...

---
+++ 04.03.HW-Ansible-OpenSuse/vagrant/provision/ansible-provision-OpenSUSE.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+zypper install ansible
+zypper install ansible-doc
---
+++ 04.03.HW-Ansible-OpenSuse/vagrant/provision/docker-provision-OpenSUSE.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+# SYSTEM PROVISION PHASE
+echo " ** Add hosts..."
+echo "192.168.99.130 docker40" | tee -a /etc/hosts
+echo "192.168.99.131 docker41" | tee -a /etc/hosts
+echo "192.168.99.132 docker42" | tee -a /etc/hosts
+echo "192.168.99.133 docker43" | tee -a /etc/hosts
+
+## CLEANUP PHASE - cleanup runc - the pre-installed conflicts with docker. Also RHEL's podman also, but there is none in UBUNTU
+# echo "** Trying to remove pre-existing docker packages"
+# zypper remove -y docker docker-engine docker.io containerd runc
+
+# APPLICATION PROVISION PHASE
+echo " ** Installing docker packages"
+# zypper update -y
+zypper refresh
+zypper install -y python-packaging
+zypper install -y docker python3-docker-compose
+
+echo " ** Enable + Start Docer"
+for action in {status,enable,start,status}; do
+        echo " --> action: ${action}"
+        systemctl "$action" docker;
+        # TODO: maybe more adequate checks
+done;
+
+# NOTE: is there an " ** Firewall" equivalent for OpenSUSE (preinstalled?)
+
+echo " ** Adding the $USER (vagrant) to the docker Group"
+getent group docker;
+# To Work hace to add the $USER(vagrant) to sudoers.d in advance
+## usermod -aG docker $USER;
+usermod -aG docker vagrant;
+getent group docker;
+
+
+## Adding Extra Tools
+# echo " ** Adding extra toools: lazydocker"
+# zypper install -y lazydocker
+## hacking it ->
+cp /vagrant/provision/lazydocker /usr/bin
+
+# # UPDATING PART
+# echo " ** Updating the system"
+# zypper refresh
+# zypper update -y
+
+###### PROVISION the Vagrant Machines
+# /vagrant/provision/'Docker Machine and Client install.sh'
