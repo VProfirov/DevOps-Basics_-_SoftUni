2f5a7e41264d6b5868843c70bb8043c7eae7addb
VProfirov <Vasil.Profirov@gmail.com>
2023-05-27 20:47:08 +0300

    fixing stuff

--- 03.Docker_Advanced/03.01.HW-docker-CentOS/vagrant/provision/provisioning-cli_history_sync.sh
+++ 03.Docker_Advanced/03.01.HW-docker-CentOS/vagrant/provision/provisioning-cli_history_sync.sh
@@ -25,14 +25,16 @@ if [ ! -h /home/vagrant/.zsh_history ] && [ ! -h /home/vagrant/.bash_history ] &
         rm /home/vagrant/.bash_history
         rm -fr /home/vagrant/bash_history
 
+        # NOTE: the fallowing single line become uselss due to the zsh_init (fix?)
         ln -s /vagrant/cli_history/.zsh_history /home/vagrant/.zsh_history;
         ln -s /vagrant/cli_history/.bash_history /home/vagrant/.bash_history;
         ln -s /vagrant/cli_history/bash_history /home/vagrant/bash_history;
 
         # NOTE: on the next vagrant ssh the .zsh_history is re-written and I should export the HISTFILE=/vagrant/cli... and source the .zshrc
-        grep -E "HISTFILE=" $HOME/.zshrc || echo 'export HISTFILE="/vagrant/cli_history/.zsh_history"' || tee -a $HOME/.zshrc
 fi
-echo "** redirecting the HISTFILE through .zshrc: /vagrant/cli_history/.zsh_history"
+
+echo "** redirecting the HISTFILE through .zshrc: /vagrant/cli_history/.zsh_history (HOME->$HOME)"
+grep -E "HISTFILE=" $HOME/.zshrc || echo 'export HISTFILE="/vagrant/cli_history/.zsh_history"' || tee -a $HOME/.zshrc
 # ls -lahtF /vagrant
 # ls -lahtF /vagrant/cli_history
 
@@ -40,7 +42,7 @@ echo "** redirecting the HISTFILE through .zshrc: /vagrant/cli_history/.zsh_hist
 # # chmod go-rw /vagrant/cli_history/.zsh_history
 # ls -lahtF /vagrant/cli_history/.zsh_history
 
-# NOTE: The fallowing does FUCKALL (aka nothing)
+# NOTE: The zsh_init is over-writing the $HISTFILE, so the zshrc should export it everytime --> needs a sed-check in the .zshrc for the HISTFILE="/vagrant/..."
 # cd /vagrant/cli_history;pwd;
 # echo "HISTFILE --> $HISTFILE"
 # #source HISTFILE="/vagrant/cli_history/.zsh_history"
